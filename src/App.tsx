e2dd04a491b1a4e5b61c46df47709a829f3e5c4a
kimdayeon37 <kdy37912@naver.com>
2024-10-15 00:18:59 +0900

    feat: data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from "react";
+import { MOCK_DATA, type MockData } from "./api/mockData";
+
+
 function App() {
+    const [products, setProducts] = useState<MockData[]>([]);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [loading, setLoading] = useState(false);
 
+    const getMockData = () => {
+      setLoading(true);
 
-  return (
-    <>
-      <h1>Infinite Scroll Example</h1>
-    </>
-  );
+      setTimeout(() => {
+        const fetchDate = MOCK_DATA;
+        setProducts(fetchDate);
+
+        // 전체 price 합산
+        const total = fetchDate.reduce((sum, product) => sum + product.price, 0);
+        setTotalPrice(total);
+
+       setLoading(false);
+      }, 1000)
+    }
+
+    useEffect(() => {
+      getMockData()
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Infinite Scroll</h1>
+            <div className="products">
+            <h2>Total Price: ${totalPrice}</h2>
+                {products.map(product => (
+                    <div key={product.productId} className="product">
+                        <h3>{product.productName}</h3>
+                        <p>Price: ${product.price}</p>
+                        <p>Date: {new Date(product.boughtDate).toLocaleDateString()}</p>
+                    </div>
+                ))}
+            </div>
+            {loading && <p>Loading...</p>}
+        </div>
+    );
 }
 
 export default App;
--- src/api/mockData.ts
+++ src/api/mockData.ts
@@ -1,4 +1,9 @@
-import { MockData } from "../types/MockData";
+export interface MockData {
+    productId: string; // 제품 ID
+    productName: string; // 제품 이름
+    price: number; // 가격
+    boughtDate: string; // 구매 날짜
+  }
 
 export const MOCK_DATA: MockData[] = [
 {
