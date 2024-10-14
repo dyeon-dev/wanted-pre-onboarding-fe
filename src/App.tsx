19ecf24c2c985a7c6fb4322ca0be2e123bc6c3e2
kimdayeon37 <kdy37912@naver.com>
2024-10-14 23:24:52 +0900

    data: mock data 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-
 function App() {
+
+
   return (
     <>
-    
+      <h1>Infinite Scroll Example</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/types/MockData.ts
@@ -0,0 +1,6 @@
+export interface MockData {
+    productId: string; // 제품 ID
+    productName: string; // 제품 이름
+    price: number; // 가격
+    boughtDate: string; // 구매 날짜
+  }
